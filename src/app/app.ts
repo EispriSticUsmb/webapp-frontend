2e294008c52b77d888f6e5609bc8cd956977d123
synnfall <synnfall@gmail.com>
2025-09-13 13:07:29 +0200

    changelent de local vers celui fr-FR

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,18 @@
-import { Component, inject } from '@angular/core';
+import localeFr from '@angular/common/locales/fr';
+import { Component, inject, LOCALE_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { AuthService } from './core/auth/auth.service';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeFr, 'fr');
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  providers: [{ provide: LOCALE_ID, useValue: 'fr-FR' }],
 })
+
 export class App {
   authService = inject(AuthService)
 
