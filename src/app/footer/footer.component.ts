364d0e85793def540e550e800eb960efefc221cb
synnfall <synnfall@gmail.com>
2025-10-24 21:44:24 +0200

    finissions du footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  year: number = (new Date()).getFullYear();
 
+  DiscordLogo: String = '/assets/logo_discord.png';
+  InstaLogo: String = '/assets/logo_instagram.png';
+
+  constructor() {
+  }
 }
